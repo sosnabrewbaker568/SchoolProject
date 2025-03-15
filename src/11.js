a54deb3c5f23c8df82863d8f2fe7de0373ee6431
sosnabrewbaker568 <sosnabrewbaker568@gmail.com>
2025-03-15 13:22:44 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,11 @@
+// JavaScript code for the school project
+
+const studentName = "John Doe";
+const studentGrade = 8;
+const studentSchool = "Elementary School";
+
+function getStudentInfo() {
+  console.log(`${studentName} is in ${studentGrade} grade at ${studentSchool}.`);
+}
+
+getStudentInfo();
