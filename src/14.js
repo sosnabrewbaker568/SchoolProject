8b698d7587f8b127f6a1805cd92583bc72537bc8
sosnabrewbaker568 <sosnabrewbaker568@gmail.com>
2025-03-18 18:25:39 +0000

    Add 14.js

---
+++ src/14.js
@@ -0,0 +1,6 @@
+function getRandomColor() {
+  var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+console.log(getRandomColor());
